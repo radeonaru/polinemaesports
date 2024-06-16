95fe0fed643e3cfa922511710a555368c5d9722f
radeonaru <radeonaru@gmail.com>
2024-06-16 20:48:07 +0700

    feat: create migration for events

---
+++ database/migrations/2024_06_16_133431_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id('id_event');
+            $table->string('nama_event');
+            $table->string('lokasi_event');
+            $table->dateTime('tanggal_event');
+            $table->text('deskripsi_event');
+            $table->text('gambar_event');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
