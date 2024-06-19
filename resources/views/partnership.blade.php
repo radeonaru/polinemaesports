cee4d5b281c55f5dcbd1c53e616645c0c5d32f70
radeonaru <radeonaru@gmail.com>
2024-06-19 22:58:28 +0700

    feat: create partnership page

---
+++ resources/views/partnership.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('content')
+    <article class="bg-white rounded-lg shadow-md overflow-hidden bg-gray-200">
+        <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:items-center lg:justify-between lg:px-8">
+            <div class="text-center">
+                <p class="mt-5 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">Meet our
+                    Partnerships
+                </p>
+            </div>
+            <div class="mt-10 space-y-10">
+                <div class="flex flex-col sm:flex-row items-start sm:items-start">
+                    <div class="p-4 flex-shrink-0 border rounded mb-4 sm:mb-0 sm:mr-4">
+                        <img src="../public/content/foto_partner/partner1.png" alt="EVOS Esports" height="200" width="200">
+                    </div>
+                    <div class="w-full sm:w-auto">
+                        <div class="box-content h-auto">
+                            <p class="m-5 text-lg text-gray-500">
+                                EVOS Esports adalah organisasi esports terkemuka di Asia Tenggara, dikenal dengan tim-tim
+                                kompetitifnya yang berprestasi dalam berbagai turnamen internasional. Dengan basis penggemar
+                                yang luas dan aktif, EVOS Esports tidak hanya unggul di arena permainan tetapi juga
+                                berkomitmen untuk membangun komunitas yang dinamis dan inovatif. Melalui dedikasi, kerja
+                                keras, dan semangat juang, EVOS Esports terus menginspirasi dan menetapkan standar baru di
+                                dunia esports.
+                            </p>
+                        </div>
+                        <hr class="border-t border-gray-300 mb-2">
+                    </div>
+                </div>
+                <div class="flex flex-col sm:flex-row items-start sm:items-start">
+                    <div class="p-4 flex-shrink-0 border rounded mb-4 sm:mb-0 sm:mr-4">
+                        <img src="../public/content/foto_partner/partner2.png" alt="MSL" height="200" width="200">
+                    </div>
+                    <div class="w-full sm:w-auto">
+                        <div class="box-content h-auto">
+                            <p class="m-5 text-lg text-gray-500">
+                                Moonton Student Leader (MSL) adalah program inisiatif dari Moonton yang bertujuan untuk
+                                memberdayakan mahasiswa dengan keterampilan kepemimpinan di bidang esports. Program ini
+                                memberikan kesempatan bagi mahasiswa untuk belajar langsung dari para profesional di
+                                industri, berpartisipasi dalam berbagai proyek esports, dan mengembangkan jaringan yang luas
+                                di komunitas esports. Melalui MSL, Moonton berkomitmen untuk mendukung dan membentuk
+                                generasi pemimpin esports masa depan yang inovatif dan berwawasan luas.
+                            </p>
+                        </div>
+                        <hr class="border-t border-gray-300 mb-2">
+                    </div>
+                </div>
+                <div class="flex flex-col sm:flex-row items-start sm:items-start">
+                    <div class="p-4 flex-shrink-0 border rounded mb-4 sm:mb-0 sm:mr-4">
+                        <img src="../public/content/foto_partner/partner3.png" alt="PMJC" height="200" width="200">
+                    </div>
+                    <div class="w-full sm:w-auto">
+                        <div class="box-content h-auto">
+                            <p class="m-5 text-lg text-gray-500">
+                                PUBG Mobile Jawara Community adalah komunitas yang dibentuk untuk menghubungkan dan
+                                memberdayakan para penggemar dan pemain PUBG Mobile di Indonesia. Komunitas ini menyediakan
+                                platform bagi anggota untuk berbagi pengalaman, mengasah keterampilan bermain, dan
+                                berpartisipasi dalam berbagai turnamen dan acara yang diselenggarakan secara rutin. Melalui
+                                kolaborasi dan interaksi yang aktif, PUBG Mobile Jawara Community bertujuan untuk membangun
+                                ekosistem esports yang solid dan mendukung pertumbuhan pemain profesional di ranah PUBG
+                                Mobile.
+                            </p>
+                        </div>
+                        <hr class="border-t border-gray-300 mb-2">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+@endsection
