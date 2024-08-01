2e17a020343cca363a2ed30d50e7fe14b356d975
radeonaru <radeonaru@gmail.com>
2024-08-01 20:17:12 +0700

    feat: add store logic on Event Controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Events;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class EventController extends Controller
 {
@@ -25,4 +26,34 @@ class EventController extends Controller
 
         return view('eventdetails', ['event' => $event]);
     }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nama_event' => 'required',
+            'lokasi_event' => 'required',
+            'tanggal_event' => 'required',
+            'deskripsi_event' => 'required',
+            'gambar_event' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+        ]);
+    
+        $gambar_event = $request->file('gambar_event');
+        $gambar_event_extension = $gambar_event->getClientOriginalExtension();
+        $gambar_event_filename = $validated['nama_event'] . date('ymdhis') . '.' . $gambar_event_extension;
+    
+        $path_foto = 'foto_event';
+    
+        $path = $gambar_event->storeAs($path_foto, $gambar_event_filename, 'public');
+    
+        Events::create([
+            'nama_event' => $validated['nama_event'],
+            'lokasi_event' => $validated['lokasi_event'],
+            'tanggal_event' => $validated['tanggal_event'],
+            'deskripsi_event' => $validated['deskripsi_event'],
+            'gambar_event' => $path,
+        ]);
+    
+        return redirect('/events')->with('success', 'Event berhasil ditambahkan!');
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('landing');
 Route::get('/events', [App\Http\Controllers\EventController::class, 'index'])->name('event');
 Route::get('/events/{id}', [App\Http\Controllers\EventController::class, 'show'])->name('event.show');
+Route::post('/events', [App\Http\Controllers\EventController::class, 'store'])->name('event.store')->middleware('auth');
 
 Route::get('/partnership', [App\Http\Controllers\PartnershipController::class, 'index'])->name('partnership');
 Route::get('/about', [App\Http\Controllers\AboutController::class, 'index'])->name('about');
