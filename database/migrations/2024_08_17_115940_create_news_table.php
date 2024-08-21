e6a57b492fcb21e2a29d75dee80e865ec29fa95e
radeonaru <radeonaru@gmail.com>
2024-08-21 14:33:42 +0700

    feat: seeder for news feature

---
+++ database/migrations/2024_08_17_115940_create_news_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id('id_news');
+            $table->string('name_news');
+            $table->string('deskripsi_news');
+            $table->text('link_news');
+            $table->text('gambar_news');
+            $table->string('tombol_news');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
